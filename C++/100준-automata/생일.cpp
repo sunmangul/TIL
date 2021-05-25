5500f2f35ab85fefa8795fdcb07e73e7c572b777
Dancat <69895365+sunmangul@users.noreply.github.com>
2021-05-25 22:23:51 +0900

    Add files via upload

---
+++ C++/100준-automata/생일.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	vector<pair<pair<int, int>, pair<int, string>>> v(n);
+	
+	for(int i=0;i<n;i++)
+	{
+		cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first;
+	}
+	
+	sort(v.begin(), v.end());
+	cout << v[n-1].second.second << "\n"<< v[0].second.second <<"\n";	
+	return 0;
+}
