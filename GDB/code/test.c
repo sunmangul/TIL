eaaaf4f319ee56dcbeca7e163dffb53d53e78bf8
sunmangul <dltmwld123@naver.com>
2021-06-04 23:58:24 +0900

    gdb test c code

---
+++ GDB/code/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void loopingFunc(int d)
+{
+    if (d == 15)
+        for (int i = 0; i < 3; i++)
+        {
+            printf("Hello world\n");
+        }
+}
+
+int main()
+{
+    int a, b, c;
+    a = 5;
+    b = 10;
+    c = a + b;
+
+    loopingFunc(c);
+    
+    return 0;
+}
